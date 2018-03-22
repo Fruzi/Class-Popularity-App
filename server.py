5af6f81590e68844e361af66d0ae46afb4d79f09
Amit <amitzim5@gmail.com>
2018-03-22 16:52:35 +0200

    server and client

--- server.py
+++ server.py
@@ -3,44 +3,48 @@ import SimpleHTTPServer
 import SocketServer
 import json
 import IPython
+import  db
 
 PORT = 8000
-#global GetStudents
 
-def GetStudents(self, *args):			
-			print "in GetStudents"
-			#print args
-			self.wfile.write(json.dumps(args))
-			self.wfile.write("\n")
-			#IPython.embed()
-			
-
+def edit_answer(self, result):
+    if result == 1:
+        print "add succeeded"
+    if result == -1:
+        print "add failed"
+    data = json.dumps(result)
+    print "result from db: " + data
+    str = "HTTP/1.1 200 OK\r\nContent-Length: {contentlen}\r\n\r\n{data}".format(contentlen=len(data), data=data)
+    print str
+    self.wfile.write(str)
+    
 class MyHttpHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def __init__(self, request, client_address, server):
-		SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+    def __init__(self, request, client_address, server):
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
 
-	def do_GET(self):
-		#IPython.embed()
-		print self.path.format
-		
-	def do_POST(self):
-		import IPython
-		import json
-		jsonobj = self.rfile.read().strip()
-		kwargs = json.loads(jsonobj)
-		func_name = self.path.format().strip('/')
-		if func_name in globals():
-			f = globals()[func_name]
-			f(self, *kwargs)
-		else:
-			print "ERROR: no such function"
-			
-		
+    def do_GET(self):
+        #IPython.embed()
+        print self.path.format
+        
+    def do_POST(self):
+        jsonobj = self.rfile.read().strip()
 
+        kwargs = json.loads(jsonobj)
+        func_name = self.path.format().strip('/')
+        print func_name
+        #IPython.embed()
+        if func_name in dir(db):
+            f = getattr(db, func_name)
+            result = f(**kwargs)
+            edit_answer(self, result)
+        else:
+            print "ERROR: no such function"
+            
 Handler = MyHttpHandler
 
 httpd = SocketServer.TCPServer(("", PORT), Handler)
 
 print "serving at port", PORT
+db.create_db()
 httpd.serve_forever()
 
